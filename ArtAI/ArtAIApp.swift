cb9e05c8e97c3332ed24f6c97de6cba352a22837
Yaroslav Orlov <ulverforest@gmail.com>
2024-01-22 15:35:00 +0200

    initial

---
+++ ArtAI/ArtAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtAIApp.swift
+//  ArtAI
+//
+//  Created by Yaroslav Orlov on 22.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ArtAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtAI/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  ArtAI
+//
+//  Created by Yaroslav Orlov on 22.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .frame(width: 300)
+        .padding()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
