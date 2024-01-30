58d5cbe541eb3888485e503e52286917d99c6b25
Yaroslav Orlov <ulverforest@gmail.com>
2024-01-30 14:38:26 +0200

    add first onboarding view state

---
+++ ArtAI/Helpers/Colors.swift
@@ -0,0 +1,12 @@
+//
+//  Colors.swift
+//  ArtAI
+//
+//  Created by Yaroslav Orlov on 30.01.2024.
+//
+
+import SwiftUI
+
+struct Colors {
+    static var lightGreen = Color("light_green")
+}
---
+++ ArtAI/Onboarding/OnboardingView.swift
@@ -0,0 +1,98 @@
+//
+//  OnboardingView.swift
+//  ArtAI
+//
+//  Created by Yaroslav Orlov on 30.01.2024.
+//
+
+import SwiftUI
+
+enum OnboardingState {
+    case first
+    case second
+    case third
+}
+
+struct OnboardingView: View {
+    var body: some View {
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            firstOnboardingState
+        }
+    }
+}
+
+private extension OnboardingView {
+    private var firstOnboardingState: some View {
+        ZStack {
+            GeometryReader { proxy in
+                Image("onboarding_1")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: proxy.size.height * 0.5)
+                    .opacity(0.6)
+            }
+            VStack(spacing: 14) {
+                Spacer()
+                Text("Create \nartwork \nwith AI")
+                    .font(.system(size: 48))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Colors.lightGreen)
+                Text("Create a ART.AI account to gain access to more creation tools, publish & curate your AI generated art!")
+                    .font(.system(size: 14))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.gray)
+                Button(action: {}) {
+                    Text("Get started")
+                        .foregroundColor(.black)
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(maxWidth: .infinity, maxHeight: 48)
+                        .background(Colors.lightGreen)
+                        .cornerRadius(4)
+                        .padding(.horizontal, 24)
+                        .padding(.top, 20)
+                }
+            }
+        }
+    }
+    
+    private var secondOnboardingState: some View {
+        VStack {
+            GeometryReader { proxy in
+                Image("onboarding_2")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: proxy.size.height * 0.5)
+                    .opacity(0.6)
+            }
+            VStack(spacing: 14) {
+                Spacer()
+                Text("Create \nartwork \nwith AI")
+                    .font(.system(size: 48))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Colors.lightGreen)
+                Text("Create a ART.AI account to gain access to more creation tools, publish & curate your AI generated art!")
+                    .font(.system(size: 14))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.gray)
+                Button(action: {}) {
+                    Text("Get started")
+                        .foregroundColor(.black)
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(maxWidth: .infinity, maxHeight: 48)
+                        .background(Colors.lightGreen)
+                        .cornerRadius(4)
+                        .padding(.horizontal, 24)
+                        .padding(.top, 20)
+                }
+            }
+        }
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
