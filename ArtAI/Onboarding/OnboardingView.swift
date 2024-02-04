0b41067d3192a25e3df260b16ef9951196311384
Yaroslav Orlov <ulverforest@gmail.com>
2024-02-04 18:59:31 +0200

    add onboarding states

--- ArtAI/Onboarding/OnboardingView.swift
+++ ArtAI/Onboarding/OnboardingView.swift
@@ -14,11 +14,20 @@ enum OnboardingState {
 }
 
 struct OnboardingView: View {
+    @State private var onboardingState: OnboardingState = .first
+    
     var body: some View {
         ZStack {
             Color.black
                 .ignoresSafeArea()
-            firstOnboardingState
+            switch onboardingState {
+            case .first:
+                firstOnboardingState
+            case .second:
+                secondOnboardingState
+            case .third:
+                thirdOnboardingState
+            }
         }
     }
 }
@@ -26,13 +35,13 @@ struct OnboardingView: View {
 private extension OnboardingView {
     private var firstOnboardingState: some View {
         ZStack {
-            GeometryReader { proxy in
+//            GeometryReader { proxy in
                 Image("onboarding_1")
                     .resizable()
                     .scaledToFill()
-                    .frame(height: proxy.size.height * 0.5)
+//                    .frame(width: proxy.size.width, height: proxy.size.height * 0.6)
                     .opacity(0.6)
-            }
+//            }
             VStack(spacing: 14) {
                 Spacer()
                 Text("Create \nartwork \nwith AI")
@@ -43,7 +52,7 @@ private extension OnboardingView {
                     .font(.system(size: 14))
                     .multilineTextAlignment(.center)
                     .foregroundColor(.gray)
-                Button(action: {}) {
+                Button(action: { onboardingState = .second }) {
                     Text("Get started")
                         .foregroundColor(.black)
                         .font(.system(size: 16, weight: .semibold))
@@ -63,7 +72,40 @@ private extension OnboardingView {
                 Image("onboarding_2")
                     .resizable()
                     .scaledToFill()
-                    .frame(height: proxy.size.height * 0.5)
+//                    .frame(width: proxy.size.width, height: proxy.size.height * 0.6)
+                    .opacity(0.6)
+            }
+            VStack(spacing: 14) {
+                Spacer()
+                Text("Create \nartwork \nwith AI")
+                    .font(.system(size: 48))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Colors.lightGreen)
+                Text("Create a ART.AI account to gain access to more creation tools, publish & curate your AI generated art!")
+                    .font(.system(size: 14))
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.gray)
+                Button(action: { onboardingState = .third }) {
+                    Text("Get started")
+                        .foregroundColor(.black)
+                        .font(.system(size: 16, weight: .semibold))
+                        .frame(maxWidth: .infinity, maxHeight: 48)
+                        .background(Colors.lightGreen)
+                        .cornerRadius(4)
+                        .padding(.horizontal, 24)
+                        .padding(.top, 20)
+                }
+            }
+        }
+    }
+    
+    private var thirdOnboardingState: some View {
+        VStack {
+            GeometryReader { proxy in
+                Image("onboarding_3")
+                    .resizable()
+                    .scaledToFill()
+//                    .frame(height: proxy.size.height * 0.5)
                     .opacity(0.6)
             }
             VStack(spacing: 14) {
