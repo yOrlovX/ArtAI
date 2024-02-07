accc9d33a4bfb9db72531ebcf10be0a4bd525fbb
Yaroslav Orlov <ulverforest@gmail.com>
2024-02-07 10:52:29 +0200

    add ProfileView prototype

--- ArtAI/Profile/ProfileView.swift
+++ ArtAI/Profile/ProfileView.swift
@@ -8,48 +8,79 @@
 import SwiftUI
 
 struct ProfileView: View {
-    @State private var selectedCategory = "Created"
-    let categories = ["Created", "Liked"]
+    @State private var selectedCategory: PhotoCategories = .created
+    let itemHeight = UIScreen.main.bounds.width / 2.3 - 30
+    let columns = [GridItem(.adaptive(minimum: 100), spacing: 10), GridItem(.adaptive(minimum: 100))]
     
+    var gridImages = [1,2,3,4,5,6]
+    
+    init() {
+        UISegmentedControl.appearance().selectedSegmentTintColor = UIColor(named: "light_green")
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.black], for: .selected)
+        UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor.green], for: .normal)
+    }
     var body: some View {
         ZStack {
             Color.black
                 .ignoresSafeArea()
             VStack {
-                HStack {
-                    Spacer()
-                    Image(systemName: "gearshape")
-                        .resizable()
-                        .frame(width: 36, height: 36)
-                        .foregroundColor(.white)
+                ScrollView {
+                    VStack {
+                        HStack {
+                            Spacer()
+                            Image(systemName: "gearshape")
+                                .resizable()
+                                .frame(width: 36, height: 36)
+                                .foregroundColor(.white)
+                                .padding(.horizontal, 24)
+                        }
+                        Circle()
+                            .foregroundColor(.white)
+                            .frame(width: 102, height: 102)
+                        HStack(spacing: 32) {
+                            Text("1 Followers")
+                                .foregroundColor(.white)
+                            Text("1 Following")
+                                .foregroundColor(.white)
+                        }
+                        Picker("", selection: $selectedCategory) {
+                            ForEach(PhotoCategories.allCases, id: \.self) { category in
+                                Text(category.rawValue)
+                                
+                            }
+                        }
+                        .frame(height: 48)
+                        .pickerStyle(.segmented)
                         .padding(.horizontal, 24)
-                }
-                Circle()
-                    .foregroundColor(.white)
-                    .frame(width: 102, height: 102)
-                
-                HStack(spacing: 32) {
-                    Text("1 Followers")
-                        .foregroundColor(.white)
-                    Text("1 Following")
-                        .foregroundColor(.white)
-                }
-                Picker("", selection: $selectedCategory) {
-                    ForEach(categories, id: \.self) { category in
-                        Text(category)
-                            
+                        
+                        LazyVGrid(columns: columns, spacing: 10) {
+                            ForEach(gridImages.indices, id: \.self) { index in
+                                Rectangle()
+                                    .frame(height: index % 3 == 1 ? itemHeight * 2 : itemHeight)
+                                    .foregroundColor(.green)
+                            }
+                        }
                     }
                 }
-                .pickerStyle(.segmented)
-                .colorMultiply(selectedCategory == "Created" ? Colors.lightGreen : .red)
-                
             }
         }
     }
 }
 
+enum PhotoCategories: String, CaseIterable {
+    case created = "Created"
+    case liked = "Liked"
+}
+
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
+    }
+}
+
+extension UISegmentedControl {
+    override open func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        self.setContentHuggingPriority(.defaultLow, for: .vertical)
     }
 }
